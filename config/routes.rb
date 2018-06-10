d36dcdb3ae9593bce7985a301c13e9a2ebddf66a
henrifalves <henri.falves@hotmail.com>
2018-06-10 09:55:00 -0300

    melhoria de rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :clientes
+  resources :clientes, path: './'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
