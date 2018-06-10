4b445e0815a792e93c28d772b06087bfe6405764
henrifalves <henri.falves@hotmail.com>
2018-06-10 18:59:48 -0300

    melhoria da mensagem de validação

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -3,5 +3,5 @@ class Cliente < ApplicationRecord
     validates_presence_of :phone, message: 'não pode ser deixado em branco'
     validates_length_of :phone, maximum: 15, message: 'deve ter até 11 caracteres'
     validates_format_of :phone, :with => /[(][1-9][1=9][)] [9][0-9]{4}-[0-9]{4}/m, 
-    message: 'o formato de telefone é invalido, ele deve ser (XX) 9XXXX-XXXX'
+    message: 'Formato invalido, ele deve ser (XX) 9XXXX-XXXX'
 end
